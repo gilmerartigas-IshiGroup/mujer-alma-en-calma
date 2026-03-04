8625f6365e658f299c744048824e6bdf14b424e0
Gilmer José Artigas López <gilmerartigas@gmail.com>
2026-03-03 20:43:03 -0500

    fix: disable next image optimizer for closed preview auth

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true,
     formats: ["image/avif", "image/webp"],
     deviceSizes: [360, 390, 414, 640, 768, 1024, 1280],
     imageSizes: [96, 160, 240, 320, 420, 560, 640],
